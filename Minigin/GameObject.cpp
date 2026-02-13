b3e0e73acb329db1a47b9690f39c0fd3a6ef000e
FinnVerbeeck <239849759+CatInSand@users.noreply.github.com>
2026-02-13 19:07:08 +0100

    Added Components to GameObject
    Component removal is instant and does not yet use a destroy queue.

--- Minigin/GameObject.cpp
+++ Minigin/GameObject.cpp
@@ -3,9 +3,18 @@
 #include "ResourceManager.h"
 #include "Renderer.h"
 
+#include <algorithm>
+#include "DeltaTime.h"
+
 dae::GameObject::~GameObject() = default;
 
-void dae::GameObject::Update(){}
+void dae::GameObject::Update()
+{
+	for (std::unique_ptr<dae::Component>& pComponent : m_pComponents)
+	{
+		pComponent->Update();
+	}
+}
 
 void dae::GameObject::Render() const
 {
@@ -21,4 +30,42 @@ void dae::GameObject::SetTexture(const std::string& filename)
 void dae::GameObject::SetPosition(float x, float y)
 {
 	m_transform.SetPosition(x, y, 0.0f);
+}
+
+void dae::GameObject::AddComponent(std::unique_ptr<dae::Component>&& pComponent)
+{
+	m_pComponents.push_back(std::move(pComponent));
+}
+void dae::GameObject::RemoveComponent(const std::string& id)
+{
+	//
+	//ADD DELETE QUEUE!!!
+	//
+	m_pComponents.erase(std::remove_if(m_pComponents.begin(), m_pComponents.end(),
+		[id](const std::unique_ptr<dae::Component>& component) { return component->m_ID == id; }
+	));
+}
+std::unique_ptr<dae::Component>& dae::GameObject::GetComponent(const std::string& id)
+{
+	auto it = std::find_if(m_pComponents.begin(), m_pComponents.end(),
+		[id](const std::unique_ptr<dae::Component>& component) { return component->m_ID == id; }
+	);
+
+	assert(it == m_pComponents.end());
+	return *it;
+}
+bool dae::GameObject::HasComponent(const std::string& id)
+{
+	const auto it = std::find_if(m_pComponents.cbegin(), m_pComponents.cend(),
+		[id](const std::unique_ptr<dae::Component>& component) { return component->m_ID == id; }
+		);
+
+	if (it != m_pComponents.cend())
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
--- Minigin/GameObject.h
+++ Minigin/GameObject.h
@@ -3,25 +3,37 @@
 #include <memory>
 #include "Transform.h"
 
+#include "Component.h"
+#include <vector>
+
 namespace dae
 {
 	class Texture2D;
 	class GameObject 
 	{
-		Transform m_transform{};
-		std::shared_ptr<Texture2D> m_texture{};
 	public:
-		virtual void Update();
-		virtual void Render() const;
-
-		void SetTexture(const std::string& filename);
-		void SetPosition(float x, float y);
-
 		GameObject() = default;
 		virtual ~GameObject();
 		GameObject(const GameObject& other) = delete;
 		GameObject(GameObject&& other) = delete;
 		GameObject& operator=(const GameObject& other) = delete;
 		GameObject& operator=(GameObject&& other) = delete;
+
+		void AddComponent(std::unique_ptr<Component>&& pComponent);
+		void RemoveComponent(const std::string& id);
+		std::unique_ptr<Component>& GetComponent(const std::string& id);
+		bool HasComponent(const std::string& id);
+
+		virtual void Update();
+		virtual void Render() const;
+
+		void SetTexture(const std::string& filename);
+		void SetPosition(float x, float y);
+
+	private:
+		Transform m_transform{};
+		std::shared_ptr<Texture2D> m_texture{};
+
+		std::vector<std::unique_ptr<Component>> m_pComponents{};
 	};
 }
