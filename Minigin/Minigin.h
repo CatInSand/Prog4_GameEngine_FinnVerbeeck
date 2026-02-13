dc4d8288b053ac65da1d9a79a099eeed4fbbfd56
FinnVerbeeck <239849759+CatInSand@users.noreply.github.com>
2026-02-13 20:20:01 +0100

    Cleaned up code

--- Minigin/Minigin.h
+++ Minigin/Minigin.h
@@ -11,12 +11,10 @@ namespace dae
 		explicit Minigin(const std::filesystem::path& dataPath);
 		~Minigin();
 		void Run(const std::function<void()>& load);
-		//void RunOneFrame();
 
 		Minigin(const Minigin& other) = delete;
 		Minigin(Minigin&& other) = delete;
 		Minigin& operator=(const Minigin& other) = delete;
 		Minigin& operator=(Minigin&& other) = delete;
-
 	};
 }
