491a4a415c36b8d46fe8868daf96e3e31e8bcf15
FinnVerbeeck <239849759+CatInSand@users.noreply.github.com>
2026-03-11 19:21:31 +0100

    Added pImpl pattern for XInput

--- Minigin/InputState.cpp
+++ Minigin/InputState.cpp
@@ -1,6 +1,49 @@
 #include "InputState.h"
 #include <SDL3/SDL.h>
 #include <backends/imgui_impl_sdl3.h>
+#include <windows.h>
+#include <Xinput.h>
+
+class dae::InputState::GamepadImplementation final
+{
+public:
+	GamepadImplementation() = default;
+	dae::KeyState GetButtonState(unsigned int button) const
+	{
+		if (m_ButtonsPressedThisFrame & button)
+			return dae::KeyState::down;
+		else if (m_ButtonsReleasedThisFrame & button)
+			return dae::KeyState::up;
+		else if (m_GamepadStateCurrent.Gamepad.wButtons & button)
+			return dae::KeyState::pressed;
+		else
+			return dae::KeyState::none;
+	}
+	void UpdateStates()
+	{
+		CopyMemory(&m_GamepadStatePrevious, &m_GamepadStateCurrent, sizeof(XINPUT_STATE));
+		ZeroMemory(&m_GamepadStateCurrent, sizeof(XINPUT_STATE));
+		XInputGetState(m_ControllerIndex, &m_GamepadStateCurrent);
+
+		auto buttonChanges = m_GamepadStateCurrent.Gamepad.wButtons ^ m_GamepadStatePrevious.Gamepad.wButtons;
+		m_ButtonsPressedThisFrame = buttonChanges & m_GamepadStateCurrent.Gamepad.wButtons;
+		m_ButtonsReleasedThisFrame = buttonChanges & (~m_GamepadStateCurrent.Gamepad.wButtons);
+	}
+
+private:
+	XINPUT_STATE m_GamepadStateCurrent{};
+	XINPUT_STATE m_GamepadStatePrevious{};
+	unsigned int m_ButtonsPressedThisFrame{};
+	unsigned int m_ButtonsReleasedThisFrame{};
+	uint8_t m_ControllerIndex{ 0 };
+};
+
+dae::InputState::InputState()
+	: m_pImpl{ std::make_unique<GamepadImplementation>() }
+{
+
+}
+dae::InputState::~InputState() = default;
 
 bool dae::InputState::UpdateStates()
 {
@@ -42,14 +85,7 @@ bool dae::InputState::UpdateStates()
 		ImGui_ImplSDL3_ProcessEvent(&e);
 	}
 
-	//gamepad input
-	CopyMemory(&m_GamepadStatePrevious, &m_GamepadStateCurrent, sizeof(XINPUT_STATE));
-	ZeroMemory(&m_GamepadStateCurrent, sizeof(XINPUT_STATE));
-	XInputGetState(m_ControllerIndex, &m_GamepadStateCurrent);
-
-	auto buttonChanges = m_GamepadStateCurrent.Gamepad.wButtons ^ m_GamepadStatePrevious.Gamepad.wButtons;
-	m_ButtonsPressedThisFrame = buttonChanges & m_GamepadStateCurrent.Gamepad.wButtons;
-	m_ButtonsReleasedThisFrame = buttonChanges & (~m_GamepadStateCurrent.Gamepad.wButtons);
+	m_pImpl->UpdateStates();
 
 	return true;
 }
@@ -60,12 +96,5 @@ dae::KeyState dae::InputState::GetKeyState(SDL_Scancode scancode)
 }
 dae::KeyState dae::InputState::GetButtonState(unsigned int button) const
 {
-	if (m_ButtonsPressedThisFrame & button)
-		return dae::KeyState::down;
-	else if (m_ButtonsReleasedThisFrame & button)
-		return dae::KeyState::up;
-	else if (m_GamepadStateCurrent.Gamepad.wButtons & button)
-		return dae::KeyState::pressed;
-	else
-		return dae::KeyState::none;
+	return m_pImpl->GetButtonState(button);
 }
