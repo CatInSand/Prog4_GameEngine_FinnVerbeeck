1c10571a8a2dcc57f2fadb22c2f462914886db33
FinnVerbeeck <239849759+CatInSand@users.noreply.github.com>
2026-05-06 20:57:06 +0200

    Made Statemachine templated
    template type is shared data for states

--- Project/Engine/Systems/StateMachine.h
+++ Project/Engine/Systems/StateMachine.h
@@ -7,10 +7,11 @@
 #include <algorithm>
 #include <concepts>
 #include "Component.h"
+#include "GameObject.h"
 
 namespace dae
 {
-	class GameObject;
+	template<typename dataT>
 	class State
 	{
 	public:
@@ -21,28 +22,45 @@ namespace dae
 		virtual void Exit() = 0;
 
 	protected:
-		State(GameObject* pObject);
-		GameObject* m_pObject;
+		State(dataT* pData)
+			: m_pData{ pData }
+		{
+		}
+		dataT* m_pData;
 	};
 
+	template<typename dataT>
 	class StateMachine final : public Component
 	{
 	public:
-		StateMachine(GameObject* owner, std::vector<std::unique_ptr<State>>&& states, uint8_t startState);
+		StateMachine(GameObject* owner, std::vector<std::unique_ptr<State<dataT>>>&& states, std::unique_ptr<dataT>&& pData, uint8_t startState)
+			: Component(owner)
+			, m_States{ std::move(states) }
+			, m_pData{ std::move(pData) }
+		{
+			assert(startState < m_States.size());
+			assert(AllStatesUnique());
+
+			m_CurrentState = m_States[startState].get();
+			m_CurrentState->Enter();
+		}
 		~StateMachine() = default;
 		StateMachine(const StateMachine& other) = delete;
 		StateMachine(StateMachine&& other) = delete;
 		StateMachine& operator=(const StateMachine& other) = delete;
 		StateMachine& operator=(StateMachine&& other) = delete;
 
-		virtual void Update() override;
+		virtual void Update() override
+		{
+			m_CurrentState->Update();
+		}
 
 		template<typename T>
-			requires requires () { std::derived_from<T, State>; }
+			requires requires () { std::derived_from<T, State<dataT>>; }
 		void SetState()
 		{
 			auto it = std::find_if(m_States.begin(), m_States.end(),
-				[](const std::unique_ptr<State>& pState) { return dynamic_cast<T*>(pState.get()) != nullptr; }
+				[](const std::unique_ptr<State<dataT>>& pState) { return dynamic_cast<T*>(pState.get()) != nullptr; }
 			);
 
 			assert(it != m_States.end());
@@ -51,12 +69,34 @@ namespace dae
 			m_CurrentState->Enter();
 		}
 
-		State* CurrentState() const;
+		State<dataT>* CurrentState() const
+		{
+			return m_CurrentState;
+		}
 
 	private:
-		bool AllStatesUnique();
-		std::vector<std::unique_ptr<State>> m_States;
-		State* m_CurrentState{ nullptr };
+		bool AllStatesUnique()
+		{
+			int firstIndex{ 0 };
+			for (std::unique_ptr<State>& pState : m_States)
+			{
+				for (int secondIndex{ firstIndex + 1 }; secondIndex < m_States.size(); ++secondIndex)
+				{
+					//for each pair of states
+					if (typeid(*pState.get()) == typeid(*m_States[secondIndex].get()))
+					{
+						//duplicate state
+						return false;
+					}
+				}
+				++firstIndex;
+			}
+
+			return true;
+		}
+		std::vector<std::unique_ptr<State<dataT>>> m_States;
+		std::unique_ptr<dataT> m_pData;
+		State<dataT>* m_CurrentState{ nullptr };
 	};
 }
 
