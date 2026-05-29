efcb5f031f39d29b8a21db1594b0afd1b5563ccc
FinnVerbeeck <239849759+CatInSand@users.noreply.github.com>
2026-05-29 11:32:53 +0200

    Cleaned up Texture2D

--- Project/Engine/Base/Texture2D.cpp
+++ Project/Engine/Base/Texture2D.cpp
@@ -1,52 +1,48 @@
-#include <SDL3/SDL.h>
 #include "Texture2D.h"
-#include "Renderer.h"
+
 #include <stdexcept>
 
-dae::Texture2D::~Texture2D()
-{
-	SDL_DestroyTexture(m_texture);
-}
+#include <SDL3/SDL.h>
 
-glm::vec2 dae::Texture2D::GetSize() const
-{
-    float w{}, h{};
-    SDL_GetTextureSize(m_texture, &w, &h);
-    return { w, h };
-}
+#include "Renderer.h"
 
-SDL_Texture* dae::Texture2D::GetSDLTexture() const
+dae::Texture2D::Texture2D(const std::string& fullPath)
+    : m_Texture{ nullptr }
 {
-	return m_texture;
-}
+    SDL_Surface* surface{ SDL_LoadPNG(fullPath.c_str()) };
+    if (surface == nullptr)
+        throw std::runtime_error(std::string("Failed to load PNG: ") + SDL_GetError());
 
-dae::Texture2D::Texture2D(const std::string &fullPath)
-{
-    SDL_Surface* surface = SDL_LoadPNG(fullPath.c_str());
-    if (!surface)
-    {
-        throw std::runtime_error(
-            std::string("Failed to load PNG: ") + SDL_GetError()
-        );
-    }
-
-    m_texture = SDL_CreateTextureFromSurface(
+    m_Texture = SDL_CreateTextureFromSurface(
         Renderer::GetInstance().GetSDLRenderer(),
         surface
     );
 
     SDL_DestroySurface(surface);
 
-    if (!m_texture)
-    {
-        throw std::runtime_error(
-            std::string("Failed to create texture from surface: ") + SDL_GetError()
-        );
-    }
+    if (m_Texture == nullptr)
+        throw std::runtime_error(std::string("Failed to create texture from surface: ") + SDL_GetError());
 }
 
-dae::Texture2D::Texture2D(SDL_Texture* texture)	: m_texture{ texture } 
+dae::Texture2D::Texture2D(SDL_Texture* texture)
+    : m_Texture{ texture }
 {
-	assert(m_texture != nullptr);
+    assert(m_Texture != nullptr);
 }
 
+dae::Texture2D::~Texture2D()
+{
+	SDL_DestroyTexture(m_Texture);
+}
+
+SDL_Texture* dae::Texture2D::GetSDLTexture() const
+{
+	return m_Texture;
+}
+
+glm::vec2 dae::Texture2D::GetSize() const
+{
+    float w{}, h{};
+    SDL_GetTextureSize(m_Texture, &w, &h);
+    return { w, h };
+}
